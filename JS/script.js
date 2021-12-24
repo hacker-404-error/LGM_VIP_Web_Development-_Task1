0cf0823c3c5422352746ddebd6c9dfb04e7b1433
PRITAM DAS <er.pritamdas22@gmail.com>
2021-12-24 13:31:38 +0530

    Updated

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,37 @@
-var myIndex = 0;
-carousel();
+// var myIndex = 0;
+// carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 9000);    
-}
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 9000);    
+// }
+
+
+
+var myVideo = document.getElementById("video1"); 
+
+function playpause() { 
+  if (myVideo.pause()) 
+  myVideo.play(); 
+  else 
+  myVideo.pause(); 
+} 
+
+// function makeBig() { 
+//     myVideo.width = 560; 
+// } 
+
+// function makeSmall() { 
+//     myVideo.width = 320; 
+// } 
+
+// function makeNormal() { 
+//     myVideo.width = 420; 
+// }
